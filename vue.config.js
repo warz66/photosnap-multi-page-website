5211d5ee88b18ed59a9cc35c90633229077815cf
Warz66 <warzox@hotmail.com>
2020-10-23 03:06:22 +0200

    first commit

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/photosnap-multi-page-website/'
+      : '/'
+      /*publicPath: '/photosnap-multi-page-website/'*/
+  }
